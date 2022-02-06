53640bd1cf6d0dbb99bb59397f0dedafa4d76fdd
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-05 23:25:10 -0300

    feat: get images in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,31 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = 0 }) => {
+      const { data: images } = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return images;
+    },
+    {
+      getNextPageParam: lastPage => lastPage.after || null,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(page => page.data).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +50,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
